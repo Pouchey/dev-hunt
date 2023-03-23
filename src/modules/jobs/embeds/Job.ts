07f0a2613e4c0124ccc2e7e89d2b4d3df8eeb51d
Théophile <theophilerouchet@gmail.com>
2023-03-23 15:09:05 +0100

    add characters limit to description discord bot

--- src/modules/jobs/embeds/Job.ts
+++ src/modules/jobs/embeds/Job.ts
@@ -2,6 +2,15 @@ import { EmbedBuilder } from 'discord.js';
 import { Job } from '../types';
 
 export const createEmbedJob = (job: Job) => {
+
+  // limit the description to 4096 characters
+  if (job.description.length > 4096) {
+    job.description = job.description.slice(0, 4096);
+  }
+
+  
+
+
   const embed = new EmbedBuilder()
     .setTitle(job.title)
     .setDescription(job.description)
